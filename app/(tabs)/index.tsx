8e83fb9f86b23c613d7dd4fa36d00fcaea42d7c8
邓涵杰 <87105391+floatDreamWithSong@users.noreply.github.com>
2025-08-26 19:45:26 +0800

    test: refetch 示例

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,13 +2,16 @@ import { Button } from '@/components/ui/button';
 import { Icon } from '@/components/ui/icon';
 import { Text } from '@/components/ui/text';
 import { THEME } from '@/lib/theme';
-import { useQuery } from '@tanstack/react-query';
+import { focusManager, useQuery } from '@tanstack/react-query';
 import { Link, Stack } from 'expo-router';
 import { MoonStarIcon, StarIcon, SunIcon } from 'lucide-react-native';
 import { useColorScheme } from 'nativewind';
 import * as React from 'react';
-import { Image, type ImageStyle, View } from 'react-native';
+import { AppState, AppStateStatus, Image, type ImageStyle, Platform, View } from 'react-native';
 import axios from 'axios';
+import { atom, useAtom } from 'jotai';
+import { useFocusEffect } from '@react-navigation/native';
+import { useEffect } from 'react';
 
 const LOGO = {
   light: require('@/assets/images/react-native-reusables-light.png'),
@@ -36,16 +39,44 @@ const IMAGE_STYLE: ImageStyle = {
   height: 76,
   width: 76,
 };
+const previousState = atom({
+  data: '',
+  isFetching: false,
+});
+// 切换AppState，RN Query切换为 focus 或 blur 状态
+function onAppStateChange(status: AppStateStatus) {
+  if (Platform.OS !== 'web') {
+    focusManager.setFocused(status === 'active')
+  }
+}
 
 export default function HomeTab() {
+  // 监听AppState变化，切换RN query 的 focus 状态
+  useEffect(() => {
+    const subscription = AppState.addEventListener('change', onAppStateChange)
+  
+    return () => subscription.remove()
+  }, [])
   const { colorScheme } = useColorScheme();
+  const [refetchCount, setRefetchCount] = React.useState(0);
+  const [state, setState] = useAtom(previousState);
   const { data, isFetching, refetch } = useQuery({
     queryKey: ['ping'],
     queryFn: async () => {
+        setRefetchCount(refetchCount + 1);
       const res = await axios.get('http://192.168.31.75:4523/m1/4985137-4643795-default/ping');
       return JSON.stringify(res.data, null, 2);
     },
+    refetchOnWindowFocus: true
   });
+  React.useEffect(() => {
+    setState({ data: data ?? '', isFetching });
+  }, [data, isFetching]);
+  useFocusEffect(
+    React.useCallback(() => {
+      refetch();
+    }, [refetch]),
+  );
   return (
     <>
       <Stack.Screen options={SCREEN_OPTIONS[colorScheme ?? 'light']} />
@@ -74,6 +105,8 @@ export default function HomeTab() {
           </Link>
         </View>
         <Text>{data}</Text>
+        <Text>Previous State: {state.data}</Text>
+        <Text>Refetch Count: {refetchCount}</Text>
       </View>
     </>
   );
